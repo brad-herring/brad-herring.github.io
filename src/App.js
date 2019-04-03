64eb0f2407df9aa0c5dd26d0b90b2407a99ba64e
Herschel Herring <hbradherring@aol.com>
2019-04-03 18:53:08 -0400

    Added ThermoSmart git and moved card

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Portfolio</Link>}>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">Home</Link>}>
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import {  Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
   state = {
@@ -7,10 +7,18 @@ class Projects extends Component {
   }
   render() {
     return(
-      <div className="category-tabs">
-        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
-          <Tab>React</Tab>
-        </Tabs>
+      <div className="projects-grid">
+
+          <Card className="ThermoSmart" shadow={5} style={{width: '320px', height: '320px', margin: 'auto'}}>
+          <CardTitle expand style={{color: '#fff', background: 'url(https://d3ui957tjb5bqd.cloudfront.net/images/screenshots/products/24/249/249632/thermometerflaticon-o.jpg?1416876475) center / cover'}}>ThermoSmart</CardTitle>
+          <CardText>
+            A react app using API's to determine an optimal thermostat setting for your home.
+          </CardText>
+          <CardActions border>
+          <Button href="https://github.com/brad-herring/ThermoSmart"colored>GitHub</Button>
+        </CardActions>
+        </Card>
+
       </div>
     )
   }
