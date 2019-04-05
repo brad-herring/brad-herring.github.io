5ee67f399d4840e24e5a107ee1539553b6d5e8ca
Herschel Herring <hbradherring@aol.com>
2019-04-05 19:40:50 -0400

    Responsive viewport

--- src/Components/Aboutme.js
+++ src/Components/Aboutme.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 class Aboutme extends Component {
 
   render() {
     return(
-      <div>
-        <h1>About page</h1>
+      <div className="about-body">
+        <Grid className="about-grid">
+          <Cell col={12}>
+            <h1>About Me</h1>
+            <hr/>
+            <p>My name is Herschel Herring, I am a self taught programmer and
+            web developer living in the Orlando, FL area.
+
+            </p>
+          </Cell>
+        </Grid>
       </div>
-    )
+    );
   }
 }
 
